4aea06d474dbf289728cd489b2ffc94bb7eca923
Kartikay Gandhi <kartikaygandhi123@gmail.com>
2022-05-04 17:02:41 +0530

    removed errors

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { SearchCard } from "../styled";
 import { StyledShowCard } from "./ShowCard.styled";
 
 const ShowCard = ({ id, image, name, summary }) => {
